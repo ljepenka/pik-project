bae7f39c0a81fb7f8809610f7a5a0df841962398
mcarm <mcarmen5676@gmail.com>
2024-01-26 11:03:35 +0100

    added new graph

--- graph_data.py
+++ graph_data.py
@@ -7,30 +7,45 @@ def parse_line(line):
     particle_counter = float(values[2].split(':')[1])
     grid_size = int(values[1].split(':')[1])
     thread_count = int(values[5].split(':')[1])
-    return fps, particle_counter, grid_size, thread_count
+    collision_count = int(values[3].split(':')[1])
+    collision_test_count = int(values[4].split(':')[1])
+    return fps, particle_counter, grid_size, thread_count, collision_count, collision_test_count
 
-# Read data from the file
+
 file_path = 'results.txt'
 with open(file_path, 'r') as file:
     lines = file.readlines()
 
-# Parse data and create lists for FPS and PARTICLE_COUNTER
 fps_values = []
 particle_counter_values = []
+collision_count_values = []
+collision_test_count_values = []
 
 for line in lines:
-    fps, particle_counter, grid_size, thread_count = parse_line(line)
+    fps, particle_counter, grid_size, thread_count, collision_count, collision_test_count = parse_line(line)
     if particle_counter != 0:
         fps_values.append(fps)
         particle_counter_values.append(particle_counter)
+        collision_count_values.append(collision_count)
+        collision_test_count_values.append(collision_test_count)
 
-# Create and save the plot
+
+plt.figure()
 plt.plot(particle_counter_values, fps_values, marker='o')
 plt.title(f'FPS vs Particle Count (Grid Size: {grid_size}, Thread Count: {thread_count})')
 plt.xlabel('PARTICLE_COUNTER')
 plt.ylabel('FPS')
-plt.ylim(0, 65)  # Set y-axis limits
+plt.ylim(0, 65)
 
 plt.grid(True)
 plt.savefig(f'graphs/fps_vs_particle_counter_plot_{grid_size}x{grid_size}_{thread_count}.png')  # Save as PNG file
 
+collision_ratio_values = [x / y if y != 0 else 0 for x, y in zip(collision_count_values, collision_test_count_values)]
+plt.figure()
+plt.plot(particle_counter_values, collision_ratio_values)
+plt.title(f'Collision Detection Ratio vs Particle Count (Grid Size: {grid_size}, Thread Count: {thread_count})')
+plt.xlabel("Particle Count")
+plt.ylabel("Collision Detection Ratio")
+
+plt.grid(True)
+plt.savefig(f'graphs/collision_test_vs_collision_count_{grid_size}x{grid_size}_{thread_count}.png')
